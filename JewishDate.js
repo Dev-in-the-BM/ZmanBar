a85efd7202ee39adcc6b09c78b2d41f4fefd6a15
nlipsett4 <nlipsett4@gmail.com>
2025-10-12 06:38:21 +0000

    fix(JewishDate.js): Correct reference error in toJewishDate function
    The `toJewishDate` function included a variable `a` that was not defined, causing a `ReferenceError`. This has been corrected to use the `jewishMonth` variable instead.

--- JewishDate.js
+++ JewishDate.js
@@ -258,7 +258,7 @@ export function toJewishDate(date) {
   const JewishDate = {
     year: jewishYear,
     monthName: jewishMonthName,
-    month: a,
+    month: jewishMonth,
     day: jewishDateArr[2],
   };
   return JewishDate;
